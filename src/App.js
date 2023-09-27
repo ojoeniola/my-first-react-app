5da926b1db58e87032e2c447cd358c0267581d3d
ojoeniola <ojoeniolaesther@gmail.com>
2023-09-27 13:15:19 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className='container'>
+        <h2>Weather App</h2>
+        <footer>
+          This project was coded by Ojo Eniola and it is {" "}
         <a
-          className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          open sourced
         </a>
-      </header>
+      </footer>
+      </div>
     </div>
   );
 }
