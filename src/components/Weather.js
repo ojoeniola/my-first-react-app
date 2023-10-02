582e8c740fc100df3170c406c5ae0f0d4a8dae2f
ojoeniola <ojoeniolaesther@gmail.com>
2023-10-02 13:46:24 +0100

    update

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
+export default function Weather(props) {
   const [weatherData, setweatherData] = useState({ loaded: false});
   function handleResponse(response) {
     console.log(response.data);
@@ -71,8 +71,7 @@ export default function Weather() {
     );
   } else {
     const apiKey = "7059cb165caa3316bff682d263a01b1e";
-    let city = "New York";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
     return "Loading....";
   }
