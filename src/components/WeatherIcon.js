a01f531be688a9bbe824c08b4db1dfe368e79c64
ojoeniola <ojoeniolaesther@gmail.com>
2023-10-04 21:58:39 +0100

    added-con

---
+++ src/components/WeatherIcon.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactAnimatedWeather from "react-animated-weather";
+
+
+
+export default function WeatherIcon(props) {
+    return (
+      <ReactAnimatedWeather
+        icon="CLEAR_DAY"
+        color="goldenrod"
+        size={64}
+        animate={ true}
+      />
+    );
+}
